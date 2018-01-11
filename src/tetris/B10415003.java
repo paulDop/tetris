d4d974b84cf3a5bfc2743bd3f9e1aee96b633244
o--o <three3year@yahoo.com.tw>
2018-01-11 16:31:09 +0800

    update view to image style

--- src/tetris/B10415003.java
+++ src/tetris/B10415003.java
@@ -29,6 +29,10 @@ public class B10415003 extends gameView {
     private Block nowBlock;
     private Block nextBlock;
     
+    private Image imgList[] = new Image[7];
+    private Color colorList[] = {Color.cyan, Color.blue, Color.orange, Color.yellow, Color.green, Color.pink, Color.red};
+    
+    
     public B10415003() {
         this.gamecontroller = new gameController();
     }
@@ -38,6 +42,14 @@ public class B10415003 extends gameView {
         frame.setSize((15 + 3) * 26 + 10, 30 * 23 + 25);
         frame.setVisible(true);
         frame.add(this);
+        
+        imgList[0] = Toolkit.getDefaultToolkit().getImage(".\\src\\img\\block0.png");
+        imgList[1] = Toolkit.getDefaultToolkit().getImage(".\\src\\img\\block1.png");
+        imgList[2] = Toolkit.getDefaultToolkit().getImage(".\\src\\img\\block2.png");
+        imgList[3] = Toolkit.getDefaultToolkit().getImage(".\\src\\img\\block3.png");
+        imgList[4] = Toolkit.getDefaultToolkit().getImage(".\\src\\img\\block4.png");
+        imgList[5] = Toolkit.getDefaultToolkit().getImage(".\\src\\img\\block5.png");
+        imgList[6] = Toolkit.getDefaultToolkit().getImage(".\\src\\img\\block6.png");
     }
     
     public Color getBackgroundColor() {
@@ -72,12 +84,26 @@ public class B10415003 extends gameView {
     // Creates a border around the blockContainer and initializes the dropping piece
 
     private void drawPiece(Graphics g) {
-        g.setColor(nowBlock.getColor());
+        
         Point[][] shape = nowBlock.getShape();
         for (Point p : shape[nowBlock.getRotate()]) {
-            g.fillRect((p.x + pieceOrigin.x) * 26,
+            int flag = 0;
+            for (int k = 0 ; k < 7 ; k = k + 1)
+            {
+                if (nowBlock.getColor() == colorList[k])
+                {
+                    flag = 1;
+                    g.drawImage(imgList[k], (p.x + pieceOrigin.x) * 26, (p.y + pieceOrigin.y) * 26, this);
+                }
+            }
+            if (flag == 0)
+            {
+                g.setColor(nextBlock.getColor());
+                g.fillRect((p.x + pieceOrigin.x) * 26,
                     (p.y + pieceOrigin.y) * 26,
                     25, 25);
+            }
+            
         }
     }
 
@@ -85,24 +111,25 @@ public class B10415003 extends gameView {
         g.setColor(Color.WHITE);
         Point[][] shape = nextBlock.getShape();
         
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 3; j++) {
-                g.fillRect((i + 5 + 8) * 26,
-                    (j + 0 + 1) * 26,
-                    25, 25);
-            }
-        }
-        
-        g.setColor(nextBlock.getColor());
+        g.fillRect(13 * 26, 1 * 26, 150, 100);
         
         for (Point p : shape[nextBlock.getRotate()]) {
-            g.fillRect((p.x + 5 + 8) * 26,
-                    (p.y + 0 + 1) * 26,
+            int flag = 0;
+            for (int k = 0 ; k < 7 ; k = k + 1)
+            {
+                if (nextBlock.getColor() == colorList[k])
+                {
+                    flag = 1;
+                    g.drawImage(imgList[k], (p.x + 4) * 26 + 235, (p.y + 0) * 26 + 45, this);
+                }
+            }
+            if (flag == 0)
+            {
+                g.setColor(nextBlock.getColor());
+                g.fillRect((p.x + 5) * 26+235,
+                    (p.y + 0) * 26+45,
                     25, 25);
-            
-            Image img1 = Toolkit.getDefaultToolkit().getImage("test1.png");
-            g.drawImage(img1, (p.x + 5) * 26, (p.y + 0) * 26, this);
-            
+            }
         }
     }
 
@@ -112,14 +139,27 @@ public class B10415003 extends gameView {
         g.fillRect(0, 0, 26 * 12, 26 * 23);
         for (int i = 0; i < 12; i++) {
             for (int j = 0; j < 23; j++) {
-                g.setColor(blockContainer[i][j]);
-                g.fillRect(26 * i, 26 * j, 25, 25);
+                
+                int flag = 0;
+                for (int k = 0 ; k < 7 ; k = k + 1)
+                {
+                    if (blockContainer[i][j] == colorList[k])
+                    {
+                        flag = 1;
+                        g.drawImage(imgList[k], 26 * i, 26 * j, this);
+                    }
+                }
+                if (flag == 0)
+                {
+                    g.setColor(blockContainer[i][j]);
+                    g.fillRect(26 * i, 26 * j, 25, 25);
+                }
             }
         }
 
         // Display the score
         g.setColor(Color.WHITE);
-        g.fillRect(28 * 12, 125, 100, 50);
+        g.fillRect(13 * 26, 125, 150, 75);
         
         g.setColor(Color.BLACK);
         g.drawString("Next", 28 * 12, 20);
@@ -130,8 +170,18 @@ public class B10415003 extends gameView {
         if (gamemodel.isGameover())
         {
             g.setColor(Color.WHITE);
-            g.setFont(new Font("TimesNewRoman", Font.PLAIN, 32));
-            g.drawString("gameover!!", 7 * 12, 200);
+            g.setFont(new Font("Courier New", Font.PLAIN, 32));
+            g.drawString("----------------", 6, 175);
+            g.drawString("----gameover----", 6, 200);
+            g.drawString("----------------", 6, 225);
+            g.drawString("----gameover----", 6, 250);
+            g.drawString("----------------", 6, 275);
+            g.drawString("----gameover----", 6, 300);
+            g.drawString("----------------", 6, 325);
+            g.drawString("----gameover----", 6, 350);
+            g.drawString("----------------", 6, 375);
+            g.drawString("----gameover----", 6, 400);
+            g.drawString("----------------", 6, 425);
         }
         else 
         {
